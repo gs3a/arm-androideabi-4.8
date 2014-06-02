bd7881f585c7d90dcc482ddf9deea43df99fe142
Paul Beeler <pbeeler80@gmail.com>
2014-06-01 21:04:21 -0700

    Squash commits to save repo space
    arm-linux-androideabi-4.8: real initial push, ignore everything before this
    
    07-19-14 update
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    Update to gcc source date 8-30-14
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    small refresh for new cloog and isl versions
    
    update to 9-10-14 source
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    Update to 09-15-2014
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    update to 9-29-14
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    Update to 10-09-14
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    update to 10-16-14
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    Update to new test build
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    Bump build date to 10-27-2014
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    Update to 11-01-14
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    Fix arm_neon.h
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    Update to build date 11/03/14
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    Update for android lollipop 5.x
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    Update to 12-07-2014
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    Update to 1-1-2015
    
    01-12-2015
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>
    
    01-16-15
    
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

--- toolchain.mk
+++ toolchain.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2011 The Android Open Source Project
+# Copyright (C) 2015 The SaberMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,7 +11,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 LOCAL_PATH := $(call my-dir)
-
 include $(call all-makefiles-under, $(LOCAL_PATH))
